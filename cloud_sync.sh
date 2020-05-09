a6a4b3b97faec7fff6ad112ded99bb1bc368a970
Jan Schmitte <janschmitte@gmail.com>
2020-05-09 12:55:05 +0200

    customized DIALOGRC files added CLOUD_SYNC.INI for user configurations
    cloud_sync.sh
    * added network check

--- cloud_sync.sh
+++ cloud_sync.sh
@@ -1,30 +1,151 @@
 #!/bin/bash
 
-# set bigger font
-setfont Uni2-TerminusBold32x16
+# script variables
+WORKDIR=$(pwd)
+BACKTITLE="Cloud Sync (https://github.com/Jandalf81/cloud_sync)"
 
-export DIALOGRC=/home/pi/scripts/.dialogrc_red
+LOGFILE=/dev/shm/cloud_sync.log
 
-dialog \
-	--stdout \
-	--colors \
-	--backtitle "Back Title" \
-	--title "Title" \
-	--msgbox "RED" \
-	10 20
+
+# load user config file
+source ${WORKDIR}/cloud_sync.ini
+
+function setConsole () {
+	# set bigger font
+	setfont Uni2-TerminusBold32x16
+}
+
+
+function mainMenu () {
+	while true
+	do
+		CHOICE=$(dialog \
+			--stdout \
+			--colors \
+			--backtitle "${BACKTITLE}" \
+			--title "main Menu" \
+			--ok-label "OK" \
+			--cancel-label "Exit" \
+			--menu "What to do?" 25 75 20 \
+				1 "Check Internet access" \
+				2 "Choose .dialogrc"
+		)
+		
+		case "${CHOICE}" in
+			1) doInternetCheck  ;;
+			2) chooseDialogRC  ;;
+			*) exitMenu  ;;
+		esac
+	done
+}
+
+function chooseDialogRC () {
+	while true
+	do	
+		CHOICE=$(dialog \
+			--stdout \
+			--colors \
+			--backtitle "${BACKTITLE}" \
+			--title "Choose DialogRC" \
+			--ok-label "Set" \
+			--cancel-label "Back" \
+			--menu "Select a file" 25 75 20 \
+				1 ".dialogrc_blue" \
+				2 ".dialogrc_green" \
+				3 ".dialogrc_red"
+		)
+		
+		case "${CHOICE}" in
+			1) export DIALOGRC=${WORKDIR}/.dialogrc_blue  ;;
+			2) export DIALOGRC=${WORKDIR}/.dialogrc_green  ;;
+			3) export DIALOGRC=${WORKDIR}/.dialogrc_red  ;;
+			*) break  ;;
+		esac
+	done
+}
+
+
+function doInternetCheck () {
+	dialog \
+		--stdout \
+		--colors \
+		--backtitle "${BACKTITLE}" \
+		--title "Checking Internet access..." \
+		--ok-label "Back" \
+		--infobox "Checking Internet access..." 25 75
 	
-export DIALOGRC=/home/pi/scripts/.dialogrc_green
+	checkInternet
+	RESULT=$?
+	
+	case ${RESULT} in
+		0) 	export DIALOGRC=${WORKDIR}/.dialogrc_green
+			TEXT="Success!"
+			TIMEOUT=$TIMEOUT_OK  ;;
+		1)	export DIALOGRC=${WORKDIR}/.dialogrc_red
+			TEXT="Error, no Internet access detected"
+			TIMEOUT=${TIMEOUT_ERROR}  ;;
+		2)	export DIALOGRC=${WORKDIR}/.dialogrc_red
+			TEXT="Error, no LAN / WLAN access detected"
+			TIMEOUT=${TIMEOUT_ERROR}  ;;
+	esac
+	
+	echo ${TIMEOUT}
+	
+	dialog \
+		--stdout \
+		--colors \
+		--backtitle "${BACKTITLE}" \
+		--title "Checking Internet access..." \
+		--ok-label "Back" \
+		--pause "${TEXT}" 25 75 ${TIMEOUT}
+	
+	mainMenu
+}
 
-dialog \
-	--stdout \
-	--colors \
-	--backtitle "Back Title" \
-	--title "Title" \
-	--msgbox "GREEN" \
-	10 20
 
-# set default font
-setfont Uni2-TerminusBold16
+function exitMenu () {
+	resetConsole
+	exit
+}
 
-# clear the screen
-clear
+# check for Internet access
+# RETURNS
+#	0 	> OK
+#	1	> LAN / WLAN connected but no Internet
+#	2	> no LAN / WLAN connected
+function checkInternet() {
+	# ping Google DNS
+	ping -q -w 1 -c 1 "8.8.8.8" > /dev/null
+	
+	if [[ $? -eq 0 ]]
+	then
+		return 0		
+	fi
+	
+	GATEWAYIP=$(ip r | grep default | cut -d " " -f 3)
+	if [ "${GATEWAYIP}" == "" ]
+	then 
+		return 2
+	fi
+	
+	ping -q -w 1 -c 1 ${GATEWAYIP} > /dev/null
+	if [[ $? -eq 0 ]]
+	then
+		return 1
+	fi
+}
+
+
+function resetConsole () {
+	# reset the default font
+	setfont Uni2-TerminusBold16
+	
+	export DIALOGRC=
+
+	# clear the screen
+	clear
+}
+
+
+setConsole
+mainMenu
